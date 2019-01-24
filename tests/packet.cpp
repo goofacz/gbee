f83f03f5fb255949036095518e08ca3f8dd1db81
Tomasz Jankowski <tomasz.jankowski.mail@gmail.com>
2019-01-24 22:26:08 +0100

    Add Field unit tests

--- tests/packet.cpp
+++ tests/packet.cpp
@@ -17,5 +17,24 @@
 // along with this program.  If not, see http://www.gnu.org/licenses/.
 //
 
+#include <cstdint>
 #include <gbee/gbee.hpp>
 #include <gtest/gtest.h>
+#include <type_traits>
+
+enum class Foo
+{
+   A = 4,
+   B,
+   C
+};
+
+using namespace gbee;
+
+TEST(Field, validate)
+{
+   using Bar = Field<Foo::B, uint16_t>;
+   EXPECT_EQ(Bar::id, Foo::B);
+   EXPECT_EQ(Bar::size, sizeof(uint16_t));
+   EXPECT_TRUE((std::is_same_v<Bar::value_type, uint16_t>) );
+}
